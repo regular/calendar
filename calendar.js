788e1408a5c823d214c7ab910715aa0fbec4d206
Jan Bölsche <regular.gonzales@googlemail.com>
2016-07-07 17:34:18 +0200

    Use constants for width and height. Use function to decide whether to turn on a pixel.

--- calendar.js
+++ calendar.js
@@ -1,6 +1,23 @@
-for (var line = 0; line < 64; line++) {
-  for (var i = 0; i < 32; i++) {
-    process.stdout.write("\u2022 ");
-  }
-  console.log();
+// jshint esnext: true
+const width = 32;
+const height = 16;
+
+function schedule(x, y) {
+    if (x == 2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// output dot matrix
+for (var row = 0; row < height; row++) {
+    for (var column  = 0; column < width; column++) {
+        if ( schedule(column, row)) {
+            process.stdout.write("\u2022 ");
+        } else {
+            process.stdout.write("x ");
+        }
+    }
+    console.log();
 }
